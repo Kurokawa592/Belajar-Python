18843242722497d86077c6120cdd5a51cda4ead0
Kurokawa592 <mhanifprasetyo592@gmail.com>
2023-12-07 20:37:46 -0800

    k

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ name = input("Whats is your name? ")
 
 if name == "Ben":
     print("You're not welcome here Evil Ben!! Get out!!")
+    exit()
+else:
+    print("Hello " + name + ", thank you so much for coming in today \n")
+
+menu = "Black Coffee, Espresso, Latte, Cappucino"
+
+print(name + " what would you like from our menu today? Her is what we are serving. \n" + menu)
+
+um = input()
+
+price = 8
+
+quantity = int(input("How many coffee would you like\n"))
+
+total = price * quantity
+
+print("Thank you. Your total is: $" + str(total))
+
+print("Sounds good " + name + ", we'll have that " + um + " ready for you in a moment.")
