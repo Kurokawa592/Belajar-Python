cd3862f7bb81b7d61d1219c57ef8f3be5f1baa18
Kurokawa592 <mhanifprasetyo592@gmail.com>
2023-12-07 20:23:48 -0800

    perubahan

--- main.py
+++ main.py
@@ -2,20 +2,5 @@ print("Hello , welcome to Coffee Shop")
 
 name = input("Whats is your name? ")
 
-print("Hello " + name + ", thank you so much for coming in today \n")
-
-menu = "Black Coffee, Espresso, Latte, Cappucino"
-
-print(name + " what would you like from our menu today? Her is what we are serving. \n" + menu)
-
-um = input()
-
-price = 8
-
-quantity = int(input("How many coffee would you like\n"))
-
-total = price * quantity
-
-print("Thank you. Your total is: $" + str(total))
-
-print("Sounds good " + name + ", we'll have that " + um + " ready for you in a moment.")
+if name == "Ben":
+    print("You're not welcome here Evil Ben!! Get out!!")
